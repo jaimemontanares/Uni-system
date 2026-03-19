06859f5a2f0a921da405d972929e4c98a1cb1c1a
jaimemontanares <112397585+jaimemontanares@users.noreply.github.com>
2026-03-19 14:56:05 +0100

    Update Student.cs
    Lagt til kommentar i kode.

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,11 +2,23 @@ using System.Collections.Generic;
 
 namespace UniversitySystem.Models
 {
+    /// <summary>
+    /// Representerer en student i systemet.
+    /// Arver grunnleggende brukerdata fra User og inneholder studentspesifikke egenskaper.
+    /// </summary>
     public class Student : User
     {
+        // Unik identifikator for studenten.
         public string StudentID { get; set; }
+         // Liste over kurs studenten er meldt opp i.
         public List<Course> EnrolledCourses { get; set; } = new List<Course>();
 
+        /// <summary>
+        /// Oppretter en ny student med navn, e-post og studentnummer.
+        /// </summary>
+        /// <param name="name">Studentens navn.</param>
+        /// <param name="email">Studentens e-postadresse.</param>
+        /// <param name="studentId">Studentens unike ID.</param>
         public Student(string name, string email, string studentId)
             : base(name, email)
         {
