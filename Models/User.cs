e8e24d71fa712ec2ccf96c94e572fed493f9f800
jaimemontanares <112397585+jaimemontanares@users.noreply.github.com>
2026-04-01 19:31:55 +0200

    feat: integrate RoleType into User model
    - added Role property to User base class
    - updated constructor to require RoleType
    - prepares system for role-based authorization

--- Models/User.cs
+++ Models/User.cs
@@ -6,25 +6,44 @@ namespace UniversitySystem.Models
     /// </summary>
     public abstract class User
     {
+        public string Id { get; set; } //ny --> trenger bedre kommentar
+        
         // Navn på brukeren.
         public string Name { get; set; }
         
         // E-post brukes som unik identifikator i systemet.
         public string Email { get; set; }
 
+        public string Username { get; set; } //ny --> trenger bedre kommentar
+        
+        public string Password { get; set; } //ny --> trenger bedre kommentar
+        
+        public RoleType Role { get; protected set; } //ny --> trenger bedre kommentar
+
         /// <summary>
-        /// Oppretter en ny bruker med navn og e-post.
+        /// Må forbedre kommentar!
         /// </summary>
         /// <param name="name">Brukerens navn.</param>
         /// <param name="email">Brukerens e-postadresse.</param>
-        protected User(string name, string email)
+        protected User(            
+            string id,
+            string name,
+            string email,
+            string username,
+            string password,
+            RoleType role)
         {
+            Id = id;
             Name = name;
             Email = email;
+            Username = username;
+            Password = password;
+            Role = role;
         }
         
         /// <summary>
-        /// Returnerer en lesbar tekstrepresentasjon av brukeren.
+        /// Returnerer en lesbar tekstrepresentasjon av brukeren. 
+        /// Må dette forbedres 01.04.26?
         /// </summary>
         public override string ToString()
         {
