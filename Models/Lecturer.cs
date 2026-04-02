7ce90a20cecc610629289335a6b874f43328865d
jaimemontanares <112397585+jaimemontanares@users.noreply.github.com>
2026-04-02 12:05:49 +0200

    feat: add Lecturer class with predefined role and position

--- Models/Lecturer.cs
+++ Models/Lecturer.cs
@@ -1,1 +1,22 @@
 namespace Uni_system.Models
+{
+public class Lecturer : Employee
+  {
+    public Lecturer(
+    string id,
+    string employeeId,
+    string name,
+    string email,
+    string username,
+    string password,
+    string department)
+    : base(
+      id,
+      employeeId,
+      name,
+      email,
+      username,
+      password,
+      department)
+  }
+}
