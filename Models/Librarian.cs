c3aaf8acfb8de46fddba8d259565383c0057474d
jaimemontanares <112397585+jaimemontanares@users.noreply.github.com>
2026-04-02 12:13:10 +0200

    feat: add Librarian class with predefined role and position

--- Models/Librarian.cs
+++ Models/Librarian.cs
@@ -1,1 +1,25 @@
 namespace Uni_system.Models
+{
+  public class Librarian : Employee
+  {
+    public Librarian(
+      string id,
+      string id,
+      string employeeId,
+      string name,
+      string email,
+      string username,
+      string password,
+      string department)
+      : base(
+            id,
+            employeeId,
+            name,
+            email,
+            username,
+            password,
+            RoleType.Librarian,
+            "Bibliotekansatt",
+            department)
+  }
+}
