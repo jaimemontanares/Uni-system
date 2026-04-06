1820130c96f6da52a232fa3a93fdf27af0fe906d
jaimemontanares <112397585+jaimemontanares@users.noreply.github.com>
2026-04-06 16:29:36 +0200

    Update Lecturer.cs
    tab space - correction

--- Models/Lecturer.cs
+++ Models/Lecturer.cs
@@ -1,22 +1,22 @@
 namespace UniversitySystem.Models
 {
-public class Lecturer : Employee
-  {
-    public Lecturer(
-    string id,
-    string employeeId,
-    string name,
-    string email,
-    string username,
-    string password,
-    string department)
-    : base(
-      id,
-      employeeId,
-      name,
-      email,
-      username,
-      password,
-      department)
-  }
+  public class Lecturer : Employee
+    {
+      public Lecturer(
+      string id,
+      string employeeId,
+      string name,
+      string email,
+      string username,
+      string password,
+      string department)
+      : base(
+        id,
+        employeeId,
+        name,
+        email,
+        username,
+        password,
+        department)
+    }
 }
