d6383fe9680cc8911984e24557cca2575bde54c7
jaimemontanares <112397585+jaimemontanares@users.noreply.github.com>
2026-04-14 16:09:13 +0200

    feat: finalize RoleType enum for authorization routing

--- Models/RoleType.cs
+++ Models/RoleType.cs
@@ -1,14 +1,13 @@
 namespace UniversitySystem.Models
 {
     /// <summary>
-    /// Definerer hvilke roller en bruker kan ha i systemet.
+    /// Angir hvilke roller en bruker kan ha i systemet.
     /// </summary>
     public enum RoleType
     {
         Student,
         ExchangeStudent,
         Lecturer,
-        Librarian,
-        Admin
+        Librarian
     }
 }
